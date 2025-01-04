83959cc44f1ea9fe956606789ae855c05c225850
trongkhanhlee26 <trongkhanhlee26@gmail.com>
2025-01-04 14:05:59 +0700

    init forgotpassword

--- apps/controllers/staff/appointmentcontroller.js
+++ apps/controllers/staff/appointmentcontroller.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const Booking = require("../../model/booking");
 const Barber = require("../../model/barber");
 
-// Middleware kiểm tra quyền nhân viên
+
 const isStaff = (req, res, next) => {
     if (req.session.user && req.session.user.role === 'barber') {
         return next();
@@ -19,8 +19,6 @@ router.get("/appointment", (req, res) => {
 });
 
 
-
-// Route render trang danh sách lịch hẹn
 router.get("/", async (req, res) => {
     try {
         const barber = await Barber.findById(req.session.barberId).populate('userId', 'fullname');
@@ -34,19 +32,17 @@ router.get("/", async (req, res) => {
 // API lấy danh sách lịch hẹn của barber
 router.get("/appointments", async (req, res) => {
     try {
-        const barberId = req.session.barberId; // Lấy barberId từ session
+        const barberId = req.session.barberId;
 
-        // Lấy danh sách lịch hẹn có barber là barber đang đăng nhập
         const appointments = await Booking.find({ barber: barberId })
             .populate("user", "fullname email phonenumber")
             .populate("service", "name price")
-            .sort({ date: 1, time: 1 }); // Sắp xếp theo ngày và giờ
+            .sort({ date: 1, time: 1 });
 
-        // Chuyển đổi để trả về ngày và giờ riêng
         const formattedAppointments = appointments.map(appointment => ({
             ...appointment.toObject(),
-            date: appointment.date.toLocaleDateString(), // Định dạng ngày
-            time: appointment.time // Giữ nguyên thời gian
+            date: appointment.date.toLocaleDateString(),
+            time: appointment.time
         }));
 
         res.json(formattedAppointments);
@@ -69,8 +65,6 @@ router.put('/appointments/confirm/:id', async (req, res) => {
             return res.status(404).send('Lịch hẹn không tồn tại.');
         }
 
-        // Có thể thêm logic để thông báo cho khách hàng về sự thay đổi trạng thái
-
         res.status(200).json(updatedAppointment);
     } catch (error) {
         console.error("Lỗi khi xác nhận lịch hẹn:", error);
@@ -90,8 +84,6 @@ router.put('/appointments/cancel/:id', async (req, res) => {
         if (!updatedAppointment) {
             return res.status(404).send('Lịch hẹn không tồn tại.');
         }
-
-        // Có thể thêm logic để thông báo cho khách hàng về sự thay đổi trạng thái
 
         res.status(200).json(updatedAppointment);
     } catch (error) {
